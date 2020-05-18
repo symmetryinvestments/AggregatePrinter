2cea920469a8cdf56bb53f6e877c40ddc4102158
Robert burner Schadek <robert.schadek@informatik.uni-oldenburg.de>
2020-05-18 11:49:11 +0100

    classes also work

--- source/aggregateprinter.d
+++ source/aggregateprinter.d
@@ -2,20 +2,27 @@ module aggregateprinter;
 
 import std.conv : to;
 import std.traits : FieldNameTuple, BaseClassesTuple;
-import std.typecons : Nullable;
+import std.typecons : Nullable, nullable;
+import std.meta : staticMap;
+import std.format;
 
 AggregatePrinter!T aggPrinter(T)(auto ref T t) {
 	return AggregatePrinter!T(&t);
 }
 
+private template ClassFieldsImpl(T) {
+	enum ClassFieldsImpl = FieldNameTuple!(T);
+}
+
 private template ClassFields(T) {
-	enum ClassFields = [FieldNameTuple!(T)];
+	enum ClassFields = [staticMap!(ClassFieldsImpl, BaseClassesTuple!T)]
+		~ [ClassFieldsImpl!T];
 }
 
 struct AggregatePrinter(T) {
 	import std.meta : staticMap;
 	static if(is(T == class)) {
-		enum mems = ClassField!T;
+		enum mems = ClassFields!T;
 	} else {
 		enum mems = FieldNameTuple!T;
 	}
@@ -33,7 +40,7 @@ struct AggregatePrinter(T) {
 			output(": ");
 			enum mOne = mems[0];
 			printerImpl(output, __traits(getMember, *this.thing, mOne));
-			foreach(mem; mems[1 .. $]) {
+			static foreach(mem; mems[1 .. $]) {
 				output(", ");
 				output(mem);
 				output(": ");
@@ -46,12 +53,19 @@ struct AggregatePrinter(T) {
 
 private void printerImpl(Out,T)(ref Out o, T t) {
 	import std.traits : isSomeString;
+	import graphql : GQLDCustomLeaf;
+	import nullablestore : NullableStore;
 	static if(is(T == Nullable!Fs, Fs...)) {
 		if(t.isNull()) {
 			o("null");
 		} else {
 			o(to!string(t.get()));
 		}
+	} else static if(is(T : GQLDCustomLeaf!K, K...)) {
+		printerImpl(o, t.value);
+	} else static if(is(T : NullableStore!G, G)) {
+		o(T.stringof);
+ 		// NullableStore stores no data
 	} else static if(isSomeString!T) {
 		o("\"");
 		o(to!string(t));
@@ -63,7 +77,6 @@ private void printerImpl(Out,T)(ref Out o, T t) {
 
 unittest {
 	import std.typecons : Nullable;
-	import std.format;
 
 	struct Foo {
 		int a;
@@ -126,5 +139,56 @@ unittest {
 
 	auto f = new SubClass(13, "Hello World", true, 1337);
 	string s = format("%s", aggPrinter(f));
-	assert(s == `SubClass(a: 13, b: "Hello World", c: true, d: null, e: 1337)`, s);
+	string exp = `SubClass(a: 13, b: "Hello World", c: true, d: null, e: 1337)`;
+	string as = format("\next: %s\ngot: %s", exp, s);
+	assert(s == exp, as);
+}
+
+unittest {
+	import std.typecons : Nullable;
+
+	struct Foo {
+		Nullable!int a;
+	}
+
+	Foo f;
+	f.a = 1337;
+	string s = format("%s", aggPrinter(f));
+	assert(s == "Foo(a: 1337)", s);
+}
+unittest {
+	import std.typecons : Nullable;
+	import graphql.uda : GQLDCustomLeaf;
+	import nullablestore;
+
+	int toInt(string s) {
+		return to!int(s);
+	}
+
+	string fromInt(int i) {
+		return to!string(i);
+	}
+
+	alias QInt = GQLDCustomLeaf!(int, fromInt, toInt);
+	alias QNInt = GQLDCustomLeaf!(Nullable!int, fromInt, toInt);
+
+	struct Foo {
+		Nullable!int a;
+		QNInt b;
+		QInt c;
+		NullableStore!(int[]) d;
+	}
+
+	Foo f;
+	f.a = 1337;
+	string s = format("%s", aggPrinter(f));
+	string exp = "Foo(a: 1337, b: null, c: 0, d: NullableStore!(int[]))";
+	assert(s == exp, format("\next: %s\ngot: %s", exp, s));
+
+	Foo f2;
+	f2.a = 1338;
+	f2.b.value = nullable(37);
+	s = format("%s", aggPrinter(f2));
+	exp = "Foo(a: 1338, b: 37, c: 0, d: NullableStore!(int[]))";
+	assert(s == exp, format("\next: %s\ngot: %s", exp, s));
 }
